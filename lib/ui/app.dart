d4bb4123eef50baa86d546ac0b74ad152ef89398
edgardo <edgardoramo2000@gmail.com>
2023-10-01 20:20:38 -0500

    first commit

---
+++ lib/ui/app.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+import 'pages/auth/login.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Material App',
+      theme: ThemeData(primarySwatch: Colors.grey,useMaterial3: true ),
+      home:const Login(),
+      routes: {
+        "/login":(context) => const Login(),
+      },
+    );
+  }
+}
