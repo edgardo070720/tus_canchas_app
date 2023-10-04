5a5fb7199dd68871e847b353659feb48243e7fb7
edgardo <edgardoramo2000@gmail.com>
2023-10-04 16:21:27 -0500

    add the IU ...auth/login

--- lib/ui/pages/auth/login.dart
+++ lib/ui/pages/auth/login.dart
@@ -1,5 +1,6 @@
-
 import 'package:flutter/material.dart';
+
+import 'widgets_login.dart';
 
 class Login extends StatelessWidget {
   const Login({
@@ -8,12 +9,89 @@ class Login extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Material App Bar'),
-      ),
-      body: const Center(
-        child: Text('Hello World'),
+    TextEditingController user_text_controller = TextEditingController();
+    TextEditingController password_text_controller = TextEditingController();
+    return Container(
+      decoration: const BoxDecoration(
+          image: DecorationImage(
+              image: AssetImage('assets/images/auth/background_login.png'),
+              fit: BoxFit.fill)),
+      child: Scaffold(
+        backgroundColor: Colors.transparent,
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Card(
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(20.0),
+              ),
+              color: Colors.white24,
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Container(
+                          width: MediaQuery.of(context).size.width * 0.4,
+                          height: MediaQuery.of(context).size.height * 0.2,
+                          decoration: const BoxDecoration(
+                              image: DecorationImage(
+                                  image: AssetImage(
+                                      'assets/images/auth/logo_login.png'),
+                                  fit: BoxFit.fill)),
+                        )
+                      ],
+                    ),
+                    TextFieldLogin(
+                        text_controller: user_text_controller,
+                        text: 'Usuario',
+                        password: false),
+                    TextFieldLogin(
+                      text_controller: password_text_controller,
+                      text: 'Contraseña',
+                      password: true,
+                    ),
+                    SizedBox(
+                      width: MediaQuery.of(context).size.width * 0.86,
+                      height: MediaQuery.of(context).size.height * 0.05,
+                      child: ElevatedButton(
+                        style: ButtonStyle(
+                            backgroundColor: MaterialStateProperty.all(
+                                const Color.fromARGB(255, 0, 154, 80))),
+                        onPressed: () {},
+                        child: const Text(
+                          'log In',
+                          textAlign: TextAlign.center,
+                          style: TextStyle(color: Colors.white,fontSize: 20,),
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(10.0),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          const Text('¿No estas registrado?',
+                              style: TextStyle(color: Colors.white)),
+                          GestureDetector(
+                              onDoubleTap: () {},
+                              child: const Text(
+                                'Registrarse',
+                                style: TextStyle(
+                                    color: Color.fromARGB(255, 0, 154, 80)),
+                              ))
+                        ],
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
---
+++ lib/ui/pages/auth/widgets_login.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class TextFieldLogin extends StatelessWidget {
+  const TextFieldLogin({
+    super.key,
+    required this.text_controller,required this.text, required this.password
+  });
+
+  final TextEditingController text_controller;
+  final String text;
+  final bool password;
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(15.0),
+      child: TextField(
+        style:const TextStyle(fontSize: 20),
+        controller: text_controller,
+        obscureText: password,
+        decoration:  InputDecoration(filled: true,fillColor: Colors.white,
+            border:const OutlineInputBorder(borderRadius: BorderRadius.all(Radius.circular(12.0))), hintText: text),
+      ),
+    );
+  }
+}
