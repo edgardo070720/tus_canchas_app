07e6f4fc2a37e8754c1db4c88bb225ff14a59e9c
calindarte <carlosalbertolindartesanto@gmail.com>
2023-10-17 22:45:37 -0500

    widget formularios

--- lib/ui/app.dart
+++ lib/ui/app.dart
@@ -1,23 +1,4 @@
 import 'package:flutter/material.dart';
-// import 'package:tus_canchas_app/ui/pages/formData/user_management_form.dart';
-
-// class MyApp extends StatelessWidget {
-//   const MyApp({Key? key}) : super(key: key);
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return MaterialApp(
-//       debugShowCheckedModeBanner: false,
-//       home: Scaffold(
-//         appBar: AppBar(
-//           title: const Text('Menu del AppBar'),
-//         ),
-//         body: const UserManagementForm(),
-//       ),
-//     );
-//   }
-// }
-
 import 'pages/auth/login.dart';
 
 class MyApp extends StatelessWidget {
