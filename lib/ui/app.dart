593aecf6e2d5ae683cee5866c84490a34e11ecb8
ING. EDGARDO RAMO <edgardoramo2000@gmail.com>
2023-10-20 11:37:33 -0500

    IU del home terminada

--- lib/ui/app.dart
+++ lib/ui/app.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:tus_canchas_app/ui/pages/auth/register.dart';
+import 'package:tus_canchas_app/ui/pages/home/home.dart';
 import 'package:tus_canchas_app/ui/pages/pay/pay.dart';
 
 import 'pages/auth/login.dart';
@@ -13,13 +14,15 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'Material App',
       theme: ThemeData(primarySwatch: Colors.grey,useMaterial3: true ),
-      initialRoute: "/user_register",
+      initialRoute: "/home",
       routes: {
         "/login":(context) => const Login(),
         "/user_register":(context) => const RegisterUser(),
 
         "/payments":(context)=>const Pay(),
         "/bookings": (context) => const Booking(),
+        "/home":(context) => const Home(),
+        
       },
     );
   }
